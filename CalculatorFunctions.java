ca357f1b53e7d8aae5d290b84ece36ce1dadaba3
l-erek <derek.liu36@gmail.com>
2020-05-30 13:44:09 -0400

    Added abs value function and ln(x) where x > 1

--- CalculatorFunctions.java
+++ CalculatorFunctions.java
@@ -5,6 +5,13 @@ public class CalculatorFunctions {
 	private static final double accuracy = 0.00001;
 	public static final double e = 2.718281828459;
 
+	public static double abs(double arg) {
+		double output = arg;
+		if(arg < 0) {
+			return -output;
+		}
+		return output;
+	}
 	public static double ln(double arg) {
 
 		double output = 0;
@@ -16,20 +23,28 @@ public class CalculatorFunctions {
 		double curValue = arg - 1;
 		if (arg < 1) {
 			int i = 1;
-			//Converge the Taylor series until we hit our desired accuracy or we hit a max number of iterations
-			while (Math.abs(Math.abs(realValue) - Math.abs(output)) >= accuracy || i == maxIterations) {
+			// Converge the Taylor series until we hit our desired accuracy or we hit a max number of iterations
+			while (CalculatorFunctions.abs(CalculatorFunctions.abs(realValue) - CalculatorFunctions.abs(output)) >= accuracy || i == maxIterations) {
+				// change Math.pow to our own pow function
 				output += Math.pow(-1, (i + 1) % 2) * curValue / i;
 				curValue *= arg - 1;
 				i++;
-				System.out.println(i);
 			}
 		} else if (arg == 1) {
 			output = 0;
-			// For values > 1 use a trapezoidal sum
 		} else if (arg > 1) {
-			output = Math.log(arg);
+			if (arg == e) {
+				return 1;
+			}
+			double i = 1;
+			double base = (arg - 1) / arg;
+			// Taylor series expansion of ln(x) centered at x > 1/2
+			while (CalculatorFunctions.abs(CalculatorFunctions.abs(realValue) - CalculatorFunctions.abs(output)) >= accuracy || i == maxIterations) {
+				// change Math.pow to our own pow function
+				output += Math.pow(base, i) / i;
+				i++;
+			}
 		}
-
 		return output;
 	}
 }
