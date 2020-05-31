4ea5d9108588f12fdbd3d118fc19a1bfe644be31
JovieL25 <lllllinbb@gmail.com>
2020-05-31 10:44:37 -0400

    ten_to_x function

---
+++ eternity_main.java
@@ -0,0 +1,15 @@
+package eternity_V_1;
+/*
+ * 
+ */
+
+public class eternity_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(ten_to_x.XtoN(10,1.0/7));
+	}
+
+	
+
+}
