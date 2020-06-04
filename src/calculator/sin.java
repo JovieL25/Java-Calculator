2780f33e657b095aa85fefd3940b9f731958d9c1
jay4j <60025980+jay4j@users.noreply.github.com>
2020-06-04 19:50:21 -0400

    change something

--- src/calculator/sin.java
+++ src/calculator/sin.java
@@ -7,34 +7,40 @@ public class sin {
 	private final double pi = 3.14159265359;
 	double result;
 	
-	public sin(double x){
-		
-		input = x;
-		sin (input, precise);
-		
-	}
-	public sin(double x, int precise){
-		
-		input = x;
-		this.precise = precise;
-		sin ( input ,precise);
-		
-	}
-	
 	/*
 	 * use Taylor series to implement sine,
 	 * precise represents the nth term in the series, the default is 10
 	 * it also represents the accuracy of the function
 	 * 
-	 */
+	 */	
 	
-	public double sin(double x, int precise){
+public double sin(double x){
 		
 		result = 0;
 		
 		int fac;
 		
 		input = x*pi/180;
+		
+		for (int i = 0; i<=precise; i++){
+			fac = 1;
+			for (int j =2; j<=2*i+1; j++){
+				fac*=j;
+			}
+			
+			result += mypow(-1.0,i)*mypow(input, 2*i+1)/fac;
+			
+		}
+		return result;
+	}
+	
+public double sinforR(double x){
+		
+		result = 0;
+		
+		int fac;
+		
+		input = x;
 		
 		for (int i = 0; i<=precise; i++){
 			fac = 1;
