4047146e10c492ae135c387181502d76a0844d3e
JovieL25 <lllllinbb@gmail.com>
2020-06-04 11:10:55 -0400

    finish main function

--- eternity_V_1/eternity_main.java
+++ eternity_V_1/eternity_main.java
@@ -1,162 +1,84 @@
-package eternity_V_1;
 /*
+* Eternity main
+ * 
+ * Version 1.0
  * 
  */
+package eternity_V_1;
 
 import java.util.ArrayList;
 import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
 
 public class eternity_main {
 
+	/**
+	 * method main is a simple user interface
+	 * @param args String
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		command_parser("");
+		Scanner user_scan = null;
+		boolean flag = true;
+		String userinput;
+		while(flag){ 			 // user interface
+			System.out.print("Write your math equation\n>>>");
+			user_scan = new Scanner(System.in);
+			userinput = user_scan.nextLine();
+			if(userinput.contains("quit")) {
+				System.out.print("Bye!");
+				flag = false;
+			}
+			else
+				command_parser(userinput);
+		}
+		user_scan.close();
+		
 	}
-
+	
+	/**
+	 * method command_parser
+	 * @param command string
+	 */
 	public static void command_parser(String command) {
 		String filtered = command.replace(" ", "");
-		/*
-		 * The order of arithmetic Parentheses Exponents Multiplication and Division
-		 * Addition and Subtraction
-		 */
-		String str = "20 + 10 + a+b +b + 20".replace(" ", "");
-		String processed = process_paren(str);
-		double result;
+		String processed = process_paren(filtered);
+		
 		if(processed.contains("Invalid"))
 			System.out.println("Invalid command!");
-
-		// System.out.println(parens_check(str));
-		// System.out.println(str.substring(str.indexOf("(")+1,str.indexOf(")")));
+		else
+			System.out.println(parse_command(processed));
 	}
-
+	
+	/**
+	 * method command_parser
+	 * @param command string
+	 */
 	public static String process_paren(String str) {
 		if (!parens_check(str))
 			return "Invalid input! Please check your parentheses";
 		else {
-
 			ArrayList<String> split_result = new ArrayList<String>();
-
+			
 			for (String a : str.split("\\(")) {
 				for (String b : a.split("\\)"))
 					split_result.add(b);
 			}
-			if (split_result.size() == 1)
-				return split_result.get(0);
-			else if (split_result.size() % 2 == 0)
-				return "Invalid input! Please check your parentheses";
+			if (split_result.size() == 2) {
+				return split_result.get(1);
+			}
 			else
 				return process_paren(split_result);
 		}
 	}
-
-	public static String process_paren(ArrayList<String> command_list) {
-		double result = 0;
-		double d;
-		String middle = command_list.get(command_list.size() / 2);
-		
-		middle = Double.toString(parse_command(middle));
-		
-		for(int i=1,j=command_list.size() / 2;i<command_list.size() / 2;i++) {
-			if(command_list.get(j-i).length()==0)
-				System.out.println(command_list.get(j+i));
-			else if(command_list.get(j+i).length()==0)
-				System.out.println(command_list.get(j+i));
-			else
-				middle = Double.toString(parse_command(command_list.get(j-i)+middle+command_list.get(j+i)));
-		}
-		
-		return middle;
-	}
-
-	public static double parse_command(String a) {
-		boolean hascommand=false;
-		int begin = 0;
-		int end = 0;
-		ArrayList<String> symbols = new ArrayList<String>();
-		symbols.add("*");
-		symbols.add("/");
-		symbols.add("+");
-		symbols.add("-");
-		
-		for(boolean command=false,int i = 0;i<a.length();i++) {
-			if(a.charAt(i)=='*') {
-				 //exponent case
-				 if(a.charAt(i+1)=='*'){
-					 
-				 }
-				 //mul case
-				 else {
-					 
-				 }
-			}
-			else if(a.charAt(i)=='/') {
-				
-			}
-			else if(a.charAt(i)=='+'){
-				
-			}
-			else if(a.charAt(i)=='-') {
-				
-			}
-			else {
-				
-			}
-			
-				
-			System.out.println();
-			a.subSequence(begin, end)
-		}
-		// exponent case
-		/*
-		 * if(par_result[0] && !(par_result[1] || par_result[2] || par_result[3]||
-		 * par_result[4])){ String[] command_split = middle.split("\\**"); d =
-		 * ten_to_x.XtoN(Double.parseDouble(command_split[0]),Double.parseDouble(
-		 * command_split[1])); for(int i =2;i<command_split.length;i++) d =
-		 * ten_to_x.XtoN(d,Double.parseDouble(command_split[i])); } //mul,div,add,sub
-		 * else {
-		 * 
-		 * }
-		 * 
-		 * for(int i=command_list.size()/2,j =1;j<command_list.size()/2;j++) {
-		 * System.out.println(command_list.get(i-j)+" "+command_list.get(i+j));
-		 * System.out.println(command_list.get(i-j).equals(command_list.get(i+j))); }
-		 * return String.valueOf(result);
-		 */
-		return 0;
-	}
-
-	public static boolean[] parse_string(String a) {
-		boolean exp = false, mul = false, div = false, add = false, sub = false;
-		boolean[] result = { exp, mul, div, add, sub };
-		if (a.contains("**"))
-			result[0] = true;
-		else if (a.contains("*"))
-			result[1] = true;
-		if (a.contains("/"))
-			result[2] = true;
-		if (a.contains("+"))
-			result[2] = true;
-		if (a.contains("-"))
-			result[2] = true;
-		return result;
-	}
-
-	public static double plus(String a) {
-		return Double.parseDouble(a.split("\\+")[0]) + Double.parseDouble(a.split("\\+")[1]);
-	}
-
-	public static double minus(String a) {
-		return Double.parseDouble(a.split("\\-")[0]) - Double.parseDouble(a.split("\\-")[1]);
-	}
-
-	public static double mul(String a) {
-		return Double.parseDouble(a.split("\\*")[0]) * Double.parseDouble(a.split("\\*")[1]);
-	}
-
-	public static double div(String a) {
-		return Double.parseDouble(a.split("\\/")[0]) / Double.parseDouble(a.split("\\/")[1]);
-	}
-
+	
+	/**
+	 * method parens_check
+	 * @param text string
+	 * @return boolean 
+	 */
 	public static boolean parens_check(String text) {
 		Stack<Character> openParens = new Stack<>();
 		for (Character ch : text.toCharArray()) {
@@ -171,6 +93,103 @@ public class eternity_main {
 			}
 		}
 		return true;
+	}
+	
+	/**
+	 * method process_paren 
+	 * @param command_list ArrayList
+	 * @return middle String
+	 */
+	public static String process_paren(ArrayList<String> command_list) {
+		
+		String middle = command_list.get(command_list.size() / 2);
+		
+		middle = Double.toString(parse_command(middle));
+		
+		for(int i=1,j=command_list.size()/2;(j+i)<command_list.size();i++) {
+			if(command_list.get(j-i).length()==0)
+				middle = Double.toString(parse_command(middle+command_list.get(j+i)));
+			else if(command_list.get(j+i).length()==0)
+				middle = Double.toString(parse_command(command_list.get(j-i)+middle));
+			else
+				middle = Double.toString(parse_command(command_list.get(j-i)+middle+command_list.get(j+i)));
+		}
+		return middle;
+	}
+
+	/**
+	 * method parse_command 
+	 * @param user_command string
+	 * @return user_command double
+	 */
+	public static double parse_command(String user_command) {
+		
+		/*
+		 * The order of arithmetic 
+		 * Parentheses 
+		 * Exponents 
+		 * Multiplication and Division
+		 * Addition and Subtraction
+		 */
+		
+		
+		//System.out.println(user_command);
+	
+		String[] temp;
+		Pattern pattern_mul_exp = Pattern.compile("(\\*\\*\\d+(\\.\\d+)?(E\\d+)?\\*\\*\\d+(\\.\\d+)?(E\\\\d+)?)");
+		Pattern pattern_exp = Pattern.compile("(\\d+(\\.\\d+)?(E\\d+)?\\*\\*\\d+(\\.\\d+)?(E\\d+)?)");
+		Pattern pattern_mul = Pattern.compile("(\\d+(\\.\\d+)?(E\\d+)?\\*\\d+(\\.\\d+)?(E\\d+)?)");
+		Pattern pattern_div = Pattern.compile("(\\d+(\\.\\d+)?(E\\d+)?\\/\\d+(\\.\\d+)?(E\\d+)?)");
+		Pattern pattern_add = Pattern.compile("(\\d+(\\.\\d+)?(E\\d+)?\\+\\d+(\\.\\d+)?(E\\d+)?)");
+		Pattern pattern_sub = Pattern.compile("(\\d+(\\.\\d+)?(E\\d+)?\\-\\d+(\\.\\d+)?(E\\d+)?)");
+		
+		Matcher matcher_mul_exp = pattern_mul_exp.matcher(user_command);
+		while(matcher_mul_exp.find()) {
+			temp = matcher_mul_exp.group().split("\\*\\*");
+			user_command=user_command.replace(matcher_mul_exp.group(), "**"+
+					Double.toString(ten_to_x.XtoN(Double.parseDouble(temp[1]), Double.parseDouble(temp[2]))));
+			matcher_mul_exp = pattern_mul_exp.matcher(user_command);
+		}
+		
+		Matcher matcher_exp = pattern_exp.matcher(user_command);
+		while(matcher_exp.find()) {
+			temp = matcher_exp.group().split("\\*\\*");
+			user_command=user_command.replace(matcher_exp.group(), 
+					Double.toString(ten_to_x.XtoN(Double.parseDouble(temp[0]), Double.parseDouble(temp[1]))));
+			matcher_exp = pattern_exp.matcher(user_command);
+		}
+		
+		Matcher matcher_mul = pattern_mul.matcher(user_command);
+		while(matcher_mul.find()) {
+			temp = matcher_mul.group().split("\\*");
+			user_command=user_command.replace(matcher_mul.group(),
+					Double.toString(Double.parseDouble(temp[0])*Double.parseDouble(temp[1])));
+			matcher_mul = pattern_mul.matcher(user_command);
+		}
+		Matcher matcher_div = pattern_div.matcher(user_command);
+		while(matcher_div.find()) {
+			temp = matcher_div.group().split("\\/");
+			user_command=user_command.replace(matcher_div.group(),
+					Double.toString(Double.parseDouble(temp[0])/Double.parseDouble(temp[1])));
+			matcher_div = pattern_div.matcher(user_command);
+		}
+		Matcher matcher_add = pattern_add.matcher(user_command);
+		while(matcher_add.find()) {
+			temp = matcher_add.group().split("\\+");
+			user_command=user_command.replace(matcher_add.group(),
+					Double.toString(Double.parseDouble(temp[0])+Double.parseDouble(temp[1])));
+			matcher_add = pattern_add.matcher(user_command);
+		}
+		
+		Matcher matcher_sub = pattern_sub.matcher(user_command);
+		while(matcher_sub.find()) {
+			temp = matcher_sub.group().split("\\-");
+			user_command=user_command.replace(matcher_sub.group(),
+					Double.toString(Double.parseDouble(temp[0])-Double.parseDouble(temp[1])));
+			matcher_sub = pattern_sub.matcher(user_command);
+		}
+		//System.out.println(user_command);
+		return Double.parseDouble(user_command);
 	}
 
 }
--- eternity_V_1/ten_to_x.java
+++ eternity_V_1/ten_to_x.java
@@ -1,13 +1,48 @@
+/*
+ * ten_to_x
+ * 
+ * Version 1.1
+ * 
+ */
 package eternity_V_1;
 
 public class ten_to_x {
+	/*
+	 * This class contains all methods required to compute the TF 10^x
+	 * The methods include
+	 * 
+	 * XtoN
+	 * ln
+	 * isRational
+	 * taylor_expand
+	 * factorial
+	 * abs
+	 */
 	
+	/**
+	 * method ln compute and return ln(x)
+	 * @param x can be an integer or rational number
+	 * @return ln(x) in double
+	 */
 	public static double ln(double x) {
-		double SP=0;
+		/*
+		 * this method takes the approximation method of finding ln
+		 * The procedure is:
+		 * 1. write x in A*10^(n-1) 1<=A<=10
+		 * 2. compute y = (A-1)/(A+1)
+		 * 3. calculate SP = 2*sigma_k=0 k->inf ((y^(2k+1))/(2k+1))
+		 * 4. ln(x) = n*log(10)+Sp
+		 * 
+		 * Notice ln(10) = 2.302585092994046
+		 * src: https://math.stackexchange.com/questions/977586/is-there-an-approximation-to-the-natural-log-function-at-large-values
+		 */
 		
-		String a = String.format("%1.30e",x);
+		double SP=0;
+		String a = String.format("%1.30e",x);  //format double to scientific notation
 		a=a.split("e")[0];
-		int n = String.valueOf(Double.parseDouble(a)).length()-2;
+		int n = String.valueOf(Double.parseDouble(a)).length()-2; 
+		/*integer n being the number of decimal after turn into scientific notation */
+		
 		double y = Double.parseDouble(a);
 		y = (y-1)/(y+1);
 		
@@ -16,11 +51,22 @@ public class ten_to_x {
 		
 		return n*2.302585092994046+SP;
 	}
-	
+
+	/**
+	 * method isRational determine
+	 * @param d can be an integer or rational number
+	 * @return true if d is rational
+	 */
 	public static boolean isRational(double d){
 		return !(d%1==0);
 	}
 	
+	/**
+	 * method taylor_expand compute and return the taylor expansion of x^n
+	 * @param x the base number
+	 * @param n the exponent number
+	 * @return taylor expansion of the inputs
+	 */
 	public static double taylor_expand(double x,double n){
 		double result = 1;
 		for(int i =1;i<20;i++){
@@ -29,6 +75,11 @@ public class ten_to_x {
 		return result;
 	}
 	
+	/**
+	 * method factorial compute and return the factorial(x)
+	 * @param x integer
+	 * @return x!
+	 */
 	public static int factorial(int x){
 		if(x == 0 || x==1)
 			return 1;
@@ -36,7 +87,17 @@ public class ten_to_x {
 			return x*factorial(x-1);
 	}
 	
+	/**
+	 * method XtoN compute and return the x^n
+	 * @param x double
+	 * @param n double could be integer or rational number
+	 * @return x^n
+	 */
 	public static double XtoN(double x ,double n){
+		/*
+		 * if n is an integer, we can iterate to get the result
+		 * if n is a rational number, we have to use the taylor expansion
+		 * */
 		if(isRational(n)){
 			return taylor_expand(x,n);
 		}
@@ -52,4 +113,14 @@ public class ten_to_x {
 			return x*XtoN(x*x,(n-1)/2);
 
 	}
+	
+	/**
+	 * method abs is to return the absolute of the value of a
+	 * @param a double
+	 * @return a
+	 */
+	public static double abs(double a) {
+        return (a <= 0.0D) ? 0.0D - a : a;
+    }
+	
 }
